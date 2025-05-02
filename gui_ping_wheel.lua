695ad6f6fce9dd6ad438b04ef795986521acf1b3
Saurtron <saurtron@riseup.net>
2025-05-02 14:17:16 +0200

    Fix reset of blend mode and make mouse 4 and 5 only trigger wheel if alt or shift not pressed.

--- gui_ping_wheel.lua
+++ gui_ping_wheel.lua
@@ -289,7 +289,7 @@ function widget:MousePress(mx, my, button)
         -- functionality of mouse build spacing is put in here, sigh
         -- check if alt is pressed
         local alt, ctrl, meta, shift = spGetModKeyState()
-        if (button == 4 or button == 5) and alt then
+        if (button == 4 or button == 5) and (alt or shift) then
             if button == 4 then
                 Spring.SendCommands("buildspacing inc")
             elseif button == 5 then
@@ -578,7 +578,7 @@ function widget:DrawScreen()
         glEndText()
 
         glLineWidth(1)
-        glBlending(false)
+        --glBlending(false)
     end
     glPopMatrix()
 end
